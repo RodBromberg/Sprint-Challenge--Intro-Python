dda8f4d12414b64bdc0d1cd87b87784684de34fd
Rod <rod.bromberg@yahoo.com>
2020-06-14 19:36:40 -0400

    reset

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,9 +1,5 @@
-# The following list comprehension exercises will make use of the
-# defined Human class.
-import math
-import re
-
-
+# The following list comprehension exercises will make use of the 
+# defined Human class. 
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -11,7 +7,6 @@ class Human:
 
     def __repr__(self):
         return f"<Human: {self.name}, {self.age}>"
-
 
 humans = [
     Human("Alice", 29),
@@ -29,50 +24,48 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [human.name for human in humans if human.name[0] == 'D']
+a = []
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = [human.name for human in humans if human.name[-1] == 'e']
+b = []
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
-#  == '^[C-G]']
 print("Starts between C and G, inclusive:")
-c = [human.name for human in humans if human.name[0] == 'C' or human.name[0] ==
-     'D' or human.name[0] == 'E' or human.name[0] == 'F' or human.name[0] == 'G']
+c = []
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = [human.age + 10 for human in humans]
+d = []
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = [f"{human.name}-{human.age}" for human in humans]
+e = []
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(human.name, human.age)
-     for human in humans if human.age >= 27 and human.age <= 32]
+f = []
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [Human(human.name.upper(), human.age + 5) for human in humans]
+g = []
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-h = [math.sqrt(human.age) for human in humans]
+import math
+h = []
 print(h)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,31 +3,21 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-
 class GroundVehicle():
-    def __init__(self, num_wheels=4):
+    def __init__(self, num_wheels):
         self.num_wheels = num_wheels
 
-    def drive(self):
-        return 'vroooom'
+    # TODO
 
 
 # Subclass Motorcycle from GroundVehicle.
-
-class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        super().__init__(num_wheels=num_wheels)
-
-    def drive(self):
-        return "BRAAAP!!"
-
+#
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
 
 vehicles = [
     GroundVehicle(),
@@ -40,5 +30,3 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
-
-print([vehicle.drive() for vehicle in vehicles])
