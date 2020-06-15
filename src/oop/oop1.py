b201d20e4b67f6b8c056d80e5faf83757f1762a0
Rod <rod.bromberg@yahoo.com>
2020-06-14 20:59:39 -0400

    mvp

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+
+class Vehicle:
+    pass
+
+# EXTENDS VEHICLE
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+# EXTENDS VEHICLE
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+# EXTENDS FLIGHTVEHICLE
+
+
+class Starship(FlightVehicle):
+    pass
+
+# EXTENDS GROUNDVEHICLE
+
+
+class Car(GroundVehicle):
+    pass
+
+# EXTENDS GROUNDVEHICLE
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# EXTENDS FLIGHTVEHICLE
+
+
+class Airplane(FlightVehicle):
+    pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,14 +1,20 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return 'vroooom'
 
+
+# Subclass Motorcycle from GroundVehicle.
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels=num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 # Subclass Motorcycle from GroundVehicle.
 #
